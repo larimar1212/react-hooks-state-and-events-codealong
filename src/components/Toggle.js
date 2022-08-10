fe1a511de6b36d7fbbec567b08422a111e6c5437
larimar1212 <larimarfondeur@gmail.com>
2022-08-10 12:32:24 -0400

    commit tree ff2a7b9cdcac0857aea16920abf0c880ce68e508

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle() { // create function
+  const [isOn, setIsOn] = useState(false) // setter function defines default mode to false
+
+  let handleClick = () => {
+    setIsOn((isOn) => !isOn) // once clicked, the button will change to true 
+  }
+// isON is false, background color = white 
+
+const color = isOn? "red" : 'white'
+
+  return (
+    <button style={{background:color}}
+     onClick={handleClick}>{isOn? "ON": 'OFF'} 
+
+    </button>
+  )
 }
+
 
 export default Toggle;
